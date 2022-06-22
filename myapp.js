aa3ae09524d52a41f8d83c7a1beb8a4d6a8d24bd
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2022-06-23 00:33:27 +0100

    initial commit

---
+++ myapp.js
@@ -0,0 +1,10 @@
+document.write('<h1> This script prints out my name, height and country on the screen </h1>')
+
+
+let myName = "Asonglefac Millenium Nzefe"
+let height = 1.60
+let country = "Cameroon"
+
+document.write('<h3> My name is ' +myName+ '</h3>')
+document.write('<h3> My height is ' +height+ 'm</h3>')
+document.write('<h3> I am from ' +country+ '</h3>')
